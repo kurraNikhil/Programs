05263ef85a4b8387cfde32d4c9fab5ccf89fb82d
kurraNikhil <kurranikhil900@gmail.com>
2023-12-08 10:23:25 +0530

    Python

--- FibbonacciSequence.py
+++
@@ -1,9 +0,0 @@
-def fib(a=0, b=1):
-    """Generator that yields Fibonacci numbers. `a` and `b` are the seed values"""
-    while True:
-        yield a
-        a, b = b, a + b
-
-f = fib()
-x = int(input("Enter number:"))
-print(', '.join(str(next(f)) for _ in range(x)))
